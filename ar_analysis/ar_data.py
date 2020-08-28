8abe591c3ca4fa6d9d61727a15648e7e79e7b7d1
hayesla <hayesla@tcd.ie>
2020-08-28 12:51:46 +0100

    cleaning ar data access

--- ar_analysis/ar_data.py
+++ ar_analysis/ar_data.py
@@ -1,26 +1,45 @@
 from sunpy.net import hek
-import pandas as pd 
-import time
 client = hek.HEKClient()
 
-tstart = '2010-01-01'
-#tend = '2010-02-01'
-tend = '2020-08-21'
+def get_ar_data(tstart, tend):
+	"""
+	Fucntion to get the active region data from NOAA SPWC by
+	querying the HEK.
+
+	Parameters
+	----------
+	tstart : ~str
+		the start time of the query
+	tend : ~str
+		the end time of the query
+
+	Returns
+	-------
+	astropy.table.Table
+		an astropy table with the data
+	"""
+
+	result = client.search(hek.attrs.Time(tstart, tend),
+	                       hek.attrs.EventType('AR'),
+	                       hek.attrs.FRM.Name == 'NOAA SWPC Observer')
+
+	# only interested in these columns from the HEK query
+	new_columns = ['ar_noaanum', 'event_starttime', 'event_endtime', 
+	               'hpc_x', 'hpc_y', 'hgs_x', 'hgs_y', 'hgc_x', 'hgc_y', 
+	               'frm_humanflag', 'frm_name',
+	               'frm_daterun', 'ar_mtwilsoncls', 'ar_mcintoshcls', 'SOL_standard', 
+	               'ar_numspots', 'area_atdiskcenter', 'area_unit']
 
 
-t1 = time.time()
-result = client.search(hek.attrs.Time(tstart, tend),
-                       hek.attrs.EventType('AR'),
-                       hek.attrs.FRM.Name == 'NOAA SWPC Observer')
-t2 = time.time()
-print(t2 - t1)
+	new_table = result[new_columns]
 
-new_columns = ['ar_noaanum', 'event_starttime', 'event_endtime', 
-               'hpc_x', 'hpc_y', 'hgs_x', 'hgs_y', 'hgc_x', 'hgc_y', 'frm_humanflag', 'frm_name',
-               'frm_daterun', 'ar_mtwilsoncls', 'ar_mcintoshcls', 'SOL_standard', 
-               'ar_numspots', 'area_atdiskcenter', 'area_unit']
+	return new_table
 
 
-new_table = result[new_columns]
+# get the data
+ar_data = get_ar_data('2010-01-01', '2020-08-21')
+# save the data as a csv file so that it can be read in again. 
+ar_data.write('all_ar_2010-2020.csv', format='csv')
 
-new_table.write('all_ar_2010-2020.csv', format='csv')
+
+
